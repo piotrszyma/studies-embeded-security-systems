20a3d5a0fff4e95e6fd1f1a3bccde80c7d8b5fa5
Piotr Szyma <thompson2908@gmail.com>
2019-03-11 22:52:11 +0100

    Tests introduced

--- problem-set-2/tests/frequency_test.py
+++ problem-set-2/tests/frequency_test.py
@@ -1,4 +1,24 @@
+import math
+import unittest
+import logging
+
+from scipy import special as scipy_special
+
 import testing_lib
 
-class FrequencyTests(testing_lib.VhdlTestCase):
-  pass
+
+class A51Tests(testing_lib.VhdlTestCase):
+  def test_frequency_monobit(self):
+    bits = self.read_output_bits()
+    sum_of_normalized_bits = sum(2 * bit - 1 for bit in bits)
+    s_obs = abs(sum_of_normalized_bits) / math.sqrt(len(bits))
+    p_value = scipy_special.erfc(s_obs / math.sqrt(2))
+    logging.info('S_n = %s', sum_of_normalized_bits)
+    logging.info('S_obs = %s', s_obs)
+    logging.info('P_value = %s', p_value)
+    assert p_value >= 0.01
+
+
+if __name__ == '__main__':
+  logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
+  unittest.main()
--- problem-set-2/tests/testing_lib.py
+++ problem-set-2/tests/testing_lib.py
@@ -1,4 +1,9 @@
 import unittest
 
+DEFAULT_FILENAME = '/tmp/a51_output.txt'
+
+
 class VhdlTestCase(unittest.TestCase):
-  pass
+  def read_output_bits(self, filename=DEFAULT_FILENAME):
+    with open(filename, 'r') as bits_file:
+      return [int(bit.strip()) for bit in bits_file.readlines()]
